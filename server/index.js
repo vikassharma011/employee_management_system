db3d32a530bb54435d846643b0ce838fc0db5d06
vikassharma011 <vikassharma9669@gmail.com>
2024-05-23 15:19:47 +0530

    la3453

--- server/index.js
+++ server/index.js
@@ -1,43 +1,46 @@
-import express from "express" ;
-import cors from 'cors'
+import express from "express";
+import cors from 'cors';
 import cookieParser from "cookie-parser";
 import { EmployeeRouter } from "./Routes/EmployeeRoute.js";
 import { adminRouter } from "./Routes/AdminRoutes.js";
 import Jwt from "jsonwebtoken";
-import path from "path"
+import path from "path";
+import { fileURLToPath } from 'url';
 
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
 const app = express();
 app.use(cors({
     origin: ["http://localhost:3000"],
     methods: ['GET', 'POST', 'PUT', "DELETE"],
     credentials: true
-}))
-app.use(cookieParser())
+}));
+app.use(cookieParser());
 app.use(express.json());
-// app.use(express.static('Public/Images'))
 app.use(express.static(path.join(__dirname, "build")));
-app.use('/auth',adminRouter);
-app.use('/employee', EmployeeRouter)
+app.use('/auth', adminRouter);
+app.use('/employee', EmployeeRouter);
 
 const verifyUser = (req, res, next) => {
     const token = req.cookies.token;
-    if(token) {
-        Jwt.verify(token, "jwt_secret_key", (err ,decoded) => {
-            if(err) return res.json({Status: false, Error: "Wrong Token"})
+    if (token) {
+        Jwt.verify(token, "jwt_secret_key", (err, decoded) => {
+            if (err) return res.json({ Status: false, Error: "Wrong Token" });
             req.id = decoded.id;
             req.role = decoded.role;
-            next()
-        })
+            next();
+        });
     } else {
-        return res.json({Status: false, Error: "Not autheticated"})
+        return res.json({ Status: false, Error: "Not authenticated" });
     }
-}
-app.get('/verify',verifyUser, (req, res)=> {
-    return res.json({Status: true, role: req.role, id: req.id})
-} )
+};
+
+app.get('/verify', verifyUser, (req, res) => {
+    return res.json({ Status: true, role: req.role, id: req.id });
+});
 
 const port = process.env.PORT || 5000;
-app.listen(port,()=>{
-    console.log("server is running") ;
-})
+app.listen(port, () => {
+    console.log(`Server is running on port ${port}`);
+});
